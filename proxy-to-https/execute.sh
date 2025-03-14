012e6515e31deea07835bd9867416a4633a36c4c
pipi <rzh881004@icloud.com>
2025-03-14 13:52:24 +0800

    Create execute.sh

---
+++ proxy-to-https/execute.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# File: execute.sh
+# Description:  This script generates a self-signed certificate for a given domain.
+# Usage:        ./execute.sh <domain>
+# Compatibility: GNU bash, version 5.2.15(1)-release (x86_64-pc-linux-gnu)
+# -----------------------------------------------------------------------------
+
+# Retrieve arguments
+domain="$1"
+
+./manage-hosts.sh add 127.0.0.1 "$domain"
+
+./port-forward.sh 443 8000
+# ./port-forward.sh 80 8000
+
+# Function to be executed when Ctrl+C is pressed
+callback() {
+  ./manage-hosts.sh remove "$domain"
+  # chmod -x ./*
+  trap - SIGINT
+  trap - EXIT
+}
+
+# Set the trap for SIGINT (Ctrl+C) and EXIT signals
+trap callback SIGINT
+trap callback EXIT
